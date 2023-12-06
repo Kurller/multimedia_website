084933e792ea6b0fcd1f7f234ae33a0a8d0e1c87
Kurller <kolaquadry@gmail.com>
2023-12-06 22:13:41 +0100

    my commit

--- multimedia_website/settings.py
+++ multimedia_website/settings.py
@@ -117,11 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-MIDDLEWARE = [
-    # ...
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    # ...
-]
+
 
 STATIC_URL = '/static/'
 # multimedia_website/settings.py
