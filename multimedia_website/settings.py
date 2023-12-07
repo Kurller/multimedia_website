a3e7bcfbe839e46a6c853ad4cae2ad0a9c7b1e4d
Kurller <kolaquadry@gmail.com>
2023-12-07 07:11:20 +0100

    my commit

--- multimedia_website/settings.py
+++ multimedia_website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,8 +81,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
+#DATABASES = {
+   #"default": dj_database_url.parse("postgres://koladb_user:pKvcfJvVa41DTH0pUy1Y81R2JEK9ockc@dpg-clolh2p46foc73c7q00g-a.oregon-postgres.render.com/koladb")
+#}
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
