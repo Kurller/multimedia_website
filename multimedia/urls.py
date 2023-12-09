73f69d807be38e70d371b5c3eb7e9b7704277612
Kurller <kolaquadry@gmail.com>
2023-12-09 15:36:27 +0100

    multimedia2

--- multimedia/urls.py
+++ multimedia/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from .views import image_list, upload_image
 
+
 urlpatterns = [
     path('', image_list, name='image_list'),
     path('upload/', upload_image, name='upload_image'),
